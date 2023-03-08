08f19ec18f7e18f9feb00ff520309232e8cbd278
andres-pulecio <andresfelipepulecio@gmail.com>
2023-03-07 20:10:25 -0500

    feat: print screan

---
+++ print-on-screen.py
@@ -0,0 +1,6 @@
+# Write a program that asks for the crewmember's name on the console and after the user enters it displays the string Welcome to Immune Universe <crewmember>!!!, where <crewmember> is the name the user entered.
+# Pedir el nombre del tripulante al usuario
+nombre = input("Introduce tu nombre: ")
+
+# Mostrar el mensaje de bienvenida personalizado
+print("¡Bienvenido al Universo Immune " + nombre + "!")
