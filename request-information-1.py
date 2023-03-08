5b991bd4ac943e83e1f5548b6356c196f7f01a51
andres-pulecio <andresfelipepulecio@gmail.com>
2023-03-07 20:15:56 -0500

    feat: request information

--- request-information-1.py
+++ request-information-1.py
@@ -1,3 +1,4 @@
+# Write a program that asks the crew member for the number of hours worked and the cost per hour. It should then display the crew member's pay on the screen.
 # Pedir al usuario el número de horas trabajadas y el coste por hora
 horas_trabajadas = float(input("Introduce el número de horas trabajadas: "))
 coste_por_hora = float(input("Introduce el coste por hora: "))
