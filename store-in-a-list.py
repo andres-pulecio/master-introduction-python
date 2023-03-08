dd5945e7923381f4333cbbe0930db14a3002b93b
andres-pulecio <andresfelipepulecio@gmail.com>
2023-03-07 20:45:33 -0500

    feat: store in a list

---
+++ store-in-a-list.py
@@ -0,0 +1,3 @@
+# Write a program that stores the sections of the building (Kitchen, Control Room, Engine Room, Evacuation Room and Warehouse) in a list and displays it on the screen.
+secciones = ["Cocina", "Sala de control", "Sala de máquinas", "Sala de evacuación", "Almacén"]
+print(secciones)
